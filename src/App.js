281c992fa4bc661c45e1dd865d9a1a296068d6a7
tofi-124 <tofikahm@yahoo.com>
2022-02-12 18:45:34 -0500

    minor change

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ const App = () => (
         </div>
       </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>2022
           <Link to="/">
-            Cryptoverse Inc.
+            Crypto Currency
           </Link> <br />
           All Rights Reserved.
         </Typography.Title>
