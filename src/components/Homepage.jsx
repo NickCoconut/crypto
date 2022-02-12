f97f93d3b86c226888fd49dfb54ad3a226b8d347
NickCoconut <gelaxyy@gmail.com>
2022-02-12 13:30:32 -0600

    initial component setup

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import millify from "millify";
+import {Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom"
+
+const { Title } = Typography
+const Homepage = () => {
+  return (
+    <>
+    <Title level={2} classname="heading">Glocal Crypto Stats</Title>
+    </>
+  )
+}
+
+export default Homepage
