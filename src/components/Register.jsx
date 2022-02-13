298edc96781fc112fde2e11371478d2b38c3f3fc
tofi-124 <tofikahm@yahoo.com>
2022-02-13 12:58:02 -0500

    front end page for login and registeration

---
+++ src/components/Register.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+const Register = () => {
+  return (
+    <>      
+    </>
+  );
+};
+export default Register;
