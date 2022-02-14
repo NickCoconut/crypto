b955b6eb863e09412ebff3c291374c344711f56d
NickCoconut <gelaxyy@gmail.com>
2022-02-13 23:35:23 -0600

    server setup

---
+++ crypto-server/db/seeds/01_users.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+users(user_name, email, password)
+VALUES
+    ('Mario', 'mario@nintendo.com', 'test'),
+    ('Luigi', 'luigi@nintendo.com', 'test'),
+    (
+        'Princess',
+        'peach@nintendo.com',
+        'test'
+    ),
+    (
+        'Daisy',
+        'daisy@nintendo.com',
+        'test'
+    ),
+    ('Donkey', 'donkey@nintendo.com', 'test');
