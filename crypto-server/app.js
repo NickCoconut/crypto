0ad4667b3217dbe79dfbbd8d4658074779bbac63
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2022-02-16 16:49:59 +0000

    app.js body-parser

--- crypto-server/app.js
+++ crypto-server/app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const db = require('./db');
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
