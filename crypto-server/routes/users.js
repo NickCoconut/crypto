ae79db1d54da171410634d7154d78cde20c300da
tofi-124 <tofikahm@yahoo.com>
2022-02-16 16:53:18 -0500

    authentcation now working

--- crypto-server/routes/users.js
+++ crypto-server/routes/users.js
@@ -1,26 +1,70 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const client = require("../db");
+const bcrypt = require("bcryptjs");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var cookieSession = require("cookie-session");
+
+router.use(
+  cookieSession({
+    name: "session",
+    keys: ["key"],
+  })
+);
 
 module.exports = router;
 
-module.exports = function(router, db, db_helpers){
-  router.get('/', (req,res) => {
-    
-    db_helpers.getallusers()
-    .then(resp => {
-      res.json({resp})
-    })
-  })
-  router.post('/',(req,res) => {
-    console.log(req.body);
-    const abc = {name: 'abc' }
-    res.json({abc})
-    return 
-  })
-  return router
-}
+module.exports = function (router, db) {
+  router.post("/register", async (req, res) => {
+    const { formDetails } = req.body;
+
+    const emailExists = await db.query(
+      `SELECT * FROM users WHERE email = $1;`,
+      [formDetails.email]
+    );
+    if (emailExists.rows[0]) {
+      console.log("The email is already registered!");
+      return res.status(400).send("The email is already registered!");
+    }
+
+    const hashedPassword = bcrypt.hashSync(formDetails.password, 10);
+
+    const newTodo = await client.query(
+      `INSERT INTO users (user_name, email, password) VALUES ($1, $2, $3) returning *;`,
+      [formDetails.username, formDetails.email, hashedPassword]
+    );
+    res.json(newTodo.rows[0]);
+  });
+
+  router.post("/login", async (req, res) => {
+
+    const { formDetails } = req.body;
+
+    const validUser = await db.query(`SELECT * FROM users WHERE email = $1;`, [
+      formDetails.email,
+    ]); //checking email from the db
+
+    if (!validUser.rows[0]) {
+      console.log("This user does not exist! You have to register!");
+      return res
+        .status(400)
+        .send("This user does not exist! You have to register!");
+    }
+
+    const passwordMatch = bcrypt.compareSync(
+      formDetails.password,
+      validUser.rows[0].password
+    ); //checking the existing password with the inserted
+
+    if (!passwordMatch) {
+      console.log("Incorrect password!");
+      return res.status(400).send("Incorrect password!");
+    }
+
+    console.log('logged on')
+
+
+  });
+
+  return router;
+};
