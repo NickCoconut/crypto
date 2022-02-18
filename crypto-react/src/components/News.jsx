4cd747eb2990990c4565f949037eebf70f2cd277
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2022-02-18 03:00:14 +0000

    comments box

--- crypto-react/src/components/News.jsx
+++ crypto-react/src/components/News.jsx
@@ -51,7 +51,10 @@ const News = ({ simplified }) => {
                   <Text className="provider-name">{news.provider[0]?.name}</Text>
                 </div>
                 <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
-                <FontAwesomeIcon icon={faHeart} onClick={() => console.log("clicked")}/>
+              </div>
+              <div className="fudge-container">
+                <FontAwesomeIcon icon={faHeart} onClick={() => console.log("clicked")} />
+                <input type="text" value={"Comment"} />
               </div>
             </a>
           </Card>
