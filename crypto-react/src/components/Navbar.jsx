0d79c5ec14d0cbb3fb5b39bd0fedeb405db209ad
tofi-124 <tofikahm@yahoo.com>
2022-02-22 12:05:10 -0500

    auth impln

--- crypto-react/src/components/Navbar.jsx
+++ crypto-react/src/components/Navbar.jsx
@@ -10,13 +10,34 @@ import {
   MenuOutlined,
   LoginOutlined,
   StarOutlined,
+  AlignLeftOutlined,
 } from "@ant-design/icons";
 
 import icon from "../images/cryptocurrency.png";
 import axios from "axios";
 
 const Navbar = () => {
-  
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   //Post Logout
   function logout() {
     axios.post("http://localhost:3001/users/logout").then((resp) => {
@@ -24,7 +45,59 @@ const Navbar = () => {
     });
   }
 
-  function loggedinuser() { //Returns navbar with Home, News, Favorites, Logout
+  function all() {
+    return (
+      <div className="nav-container">
+        <div className="logo-container">
+          <Avatar src={icon} size="large" />
+          <Typography.Title level={2} className="logo logo-text">
+            <Link to="/" className="a">
+              Crypto
+            </Link>
+          </Typography.Title>
+          <Button
+            className="menu-control-container"
+            onClick={() => setActiveMenu(!activeMenu)}
+          >
+            <MenuOutlined />
+          </Button>
+        </div>
+        {activeMenu && (
+          <Menu theme="dark">
+            <Menu.Item icon={<HomeOutlined />}>
+              <Link to="/" onClick={() => home()}>
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />}>
+              <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={<BulbOutlined />}>
+              <Link to="/news">News</Link>
+            </Menu.Item>
+            <Menu.Item icon={<StarOutlined />}>
+              <Link to="/mylikes">Favorites</Link>
+            </Menu.Item>
+            <Menu.Item icon={<LoginOutlined />}>
+              <Link to="/login">Login</Link>
+            </Menu.Item>
+            <Menu.Item icon={<UserAddOutlined />}>
+              <Link to="/register">Register</Link>
+            </Menu.Item>
+            <Menu.Item icon={<ExclamationCircleOutlined />}>
+              <Link to="/" onClick={() => logout()}>
+                Logout
+              </Link>
+            </Menu.Item>
+          </Menu>
+        )}
+      </div>
+    );
+  }
+
+  function loggedinuser() {
+    //Returns navbar with Home, News, Favorites, Logout
     return (
       <div className="nav-container">
         <div className="logo-container">
@@ -68,7 +141,8 @@ const Navbar = () => {
       </div>
     );
   }
-  function loggedoutuser() { //Returns navbar with Home, News, Login, Register
+  function loggedoutuser() {
+    //Returns navbar with Home, News, Login, Register
     return (
       <div className="nav-container">
         <div className="logo-container">
@@ -111,36 +185,23 @@ const Navbar = () => {
     );
   }
 
-  
-  
-  const [activeMenu, setActiveMenu] = useState(true);
-  const [screenSize, setScreenSize] = useState(undefined);
+  function componentDidMount() {
+    axios.post("http://localhost:3001/users/").then((resp) => {
+      return resp.data.loggedIn;
+    });
+  }
 
-  useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth);
+  let loggedIn = componentDidMount;
+  console.log(loggedIn);
 
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    if (screenSize <= 800) {
-      setActiveMenu(false);
-    } else {
-      setActiveMenu(true);
-    }
-  }, [screenSize]);
-
-  //Loggedin checker from the back end, returns true or false after checking if theres a session
-  let loggedIn =  axios.post("http://localhost:3001/users/").then((resp) => {
-    return resp.data.loggedIn;
-  })
-
-  return loggedoutuser();//renders a logged out user nav bar
-  //return loggedinuser(); //renders a logged in user nav bar
+  //if (loggedIn === true) return loggedinuser();
+  //renders a logged out user nav bar
+  //else return loggedoutuser(); //renders a logged in user nav bar
+  function check(check) {
+    if (check === true) return loggedinuser();
+    else return loggedoutuser();
+  }
+  return all();
 };
 
 export default Navbar;
